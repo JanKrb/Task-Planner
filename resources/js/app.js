1e295131f629540f9d7f4b0c391bd44a5f2baa09
Jan Ruhfus <unknownditx@gmail.com>
2021-10-03 22:08:19 +0200

    i18n setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,14 @@ import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
 import { ZiggyVue } from 'ziggy';
 import { Ziggy } from './ziggy';
+import { createI18n } from 'vue-i18n'
+import translations from './i18n'
+
+const i18n = createI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: translations
+})
 
 InertiaProgress.init()
 
@@ -12,6 +20,7 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(i18n)
             .mount(el)
     },
 })
--- resources/js/ziggy.js
+++ resources/js/ziggy.js
@@ -1,4 +1,4 @@
-const Ziggy = {"url":"http:\/\/localhost","port":null,"defaults":{},"routes":[]};
+const Ziggy = {"url":"http:\/\/127.0.0.1","port":null,"defaults":{},"routes":{"auth.render_login":{"uri":"login","methods":["GET","HEAD"]},"auth.render_register":{"uri":"register","methods":["GET","HEAD"]}}};
 
 if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
     Object.assign(Ziggy.routes, window.Ziggy.routes);
