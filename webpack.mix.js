15d8c13f7e262c28868f5320728e8dee4560bf8b
Jan Ruhfus <unknownditx@gmail.com>
2021-10-03 20:52:11 +0200

    Fix vue compiling (webpack)

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,24 +1,22 @@
 const mix = require('laravel-mix');
 const path = require('path');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
+// JS
 mix.js('resources/js/app.js', 'public/js');
+mix.vue({
+    version: 3
+})
+
+// CSS/SCSS
 mix.postCss('resources/css/app.css', 'public/css', []);
 
+
+// Alias
 mix.alias({
     ziggy: path.resolve('vendor/tightenco/ziggy/dist/vue'),
 })
 
+// Building & Prod
 if (mix.inProduction()) {
     mix.version();
 }
